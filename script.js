1a1574d4f7cd416cf267bed5ad3bf7ae3db9ef0f
ritikojha07 <167160500+ritikojha07@users.noreply.github.com>
2024-10-10 12:15:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
- var bgMusicURL = 'video/29.mp4' 
+ var bgMusicURL = '29.mp4' 
 var bgMusicControls = true; // Show UI music control
 
 
